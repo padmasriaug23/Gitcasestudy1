b8221c774cd4a6a11dcbb6abe50a4bac5c1534c9
padma <abc@gmail.com>
2020-11-11 13:00:07 +0000

    files are commited

---
+++ edurekaproj/src/main/java/co/edureka/App.java
@@ -0,0 +1,13 @@
+package co.edureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
